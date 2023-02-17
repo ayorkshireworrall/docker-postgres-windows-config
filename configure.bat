f3937579ef1f254d1502d3611f505c37fb388b4e
aworrall <alex.worrall@keytree.co.uk>
2023-02-17 20:33:18 +0000

    Formatting improvement in configure.bat

--- configure.bat
+++ configure.bat
@@ -3,5 +3,5 @@ set /p DOCKER_PG_NAME="Enter the name for your container: "
 setx DOCKER_PG_NAME %DOCKER_PG_NAME%
 set /p DOCKER_PG_VERSION="Enter the postgres version. This will search for the corresponding image on docker hub should it exist: "
 setx DOCKER_PG_VERSION %DOCKER_PG_VERSION%
-set /p DOCKER_PG_DB="Enter the database name (this should match the name you choose in your init.sql instructions)"
+set /p DOCKER_PG_DB="Enter the database name (this should match the name you choose in your init.sql instructions): "
 setx DOCKER_PG_DB %DOCKER_PG_DB%
