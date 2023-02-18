6a221bb837b8d3f5bbc496ed6b4b2bd9d4b9c492
aworrall <alex.worrall@keytree.co.uk>
2023-02-18 09:09:34 +0000

    Add restart message to config

--- configure.bat
+++ configure.bat
@@ -5,3 +5,4 @@ set /p DOCKER_PG_VERSION="Enter the postgres version. This will search for the c
 setx DOCKER_PG_VERSION %DOCKER_PG_VERSION%
 set /p DOCKER_PG_DB="Enter the database name (this should match the name you choose in your init.sql instructions): "
 setx DOCKER_PG_DB %DOCKER_PG_DB%
+echo Your Windows environment variables have been set, please restart the current command terminal for them to apply (if using an integrated IDE then you will need to restart the application)
